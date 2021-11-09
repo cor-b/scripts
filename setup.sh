d8feddd3159ebc9f5e8c8437a38d978e0c774529
hotbunga <40253612+hotbunga@users.noreply.github.com>
2021-11-09 06:16:23 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,11 +8,7 @@ sudo apt-get install secure-delete
 sudo apt-get install smem
 
 # tools
-sudo apt-get install -y html2text
-sudo apt-get install -y enum4linux
-sudo apt-get install -y sshpass
-sudo apt-get install -y steghide
-sudo apt-get install -y nasm
+sudo apt install html2text enum4linux sshpass steghide nasm netcat ncat nmap wireshark tcpdump hashcat ffuf gobuster hydra zaproxy proxychains sqlmap radare2 metasploit-framework python2.7 python3 spiderfoot theharvester remmina rdesktop crackmapexec exiftool curl seclists testssl.sh git vim tmux -y
 
 # pips
 pip install uncompyle6
